a6ab6183e2ab4e6c5fbe7a25151820526c2f9ca0
Aayush Upadhyay <aayushu@gmail.com>
2012-06-20 16:10:49 -0400

    added main.py, which is just a short program that runs the daemon

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+Temp file that lets you run the system 
+"""
+
+from mongo_internal import DaemonThread
+from doc_manager import DocManager
+from time import sleep
+
+dm = DocManager()
+dt = DaemonThread('localhost:27000', dm)
+dt.start()
+
+rows = dm.retrieve_docs()
+
+for line in rows:
+    print line
+    
+
+
+    
+
+    
