4488289e115ac220dd08dbc4e065205319b05a89
Aayush Upadhyay <aayushu@gmail.com>
2012-06-28 15:59:49 -0400

    cleaned up main

--- main.py
+++ main.py
@@ -2,15 +2,7 @@
 """
 
 from mongo_internal import Daemon
-from doc_manager import DocManager
-from time import sleep
-from pysolr import Solr
-from pymongo import Connection
-import commands
 
-#commands.getstatusoutput('rm config.txt; touch config.txt')
-#solr = Solr('http://127.0.0.1:8080/solr/')
-#solr.delete(q="*:*")        #delete everything in solr
 
 dt = Daemon('localhost:27000', 'config.txt')
 dt.start()
