675739bb05da38535c6f7a280364c37cef49793c
Leo <leonardostedile@Macintosh.local>
2012-06-29 17:20:59 -0400

    added thread stopping support in mongo_internal

--- mongo_internal.py
+++ mongo_internal.py
@@ -6,7 +6,7 @@ import time
 from threading import Thread
 from pymongo import Connection
 from oplog_manager import OplogThread
-        
+import unittest        
         
 class Daemon(Thread):
     """Checks the cluster for shards to tail. 
@@ -19,40 +19,37 @@ class Daemon(Thread):
         self.oplog_checkpoint = oplog_checkpoint
         self.address = address
         self.setDaemon(True)
-           
+        self.shard_set = {}
         
     def stop(self):
         self.running = False
-  
+        for thread in self.shard_set:
+            thread.stop()
+
   
     def run(self):
         mongos_conn = Connection(self.address)
-        shard_set = {}
+        self.assertTrue(isinstance(mongos_conn, Connection))
         shard_coll = mongos_conn['config']['shards']
+        self.assertTrue(isinstance(shard_coll, Collection))
         self.running = True
-        
         
         while self.running is True: 
             
             for shard_doc in shard_coll.find():
                 shard_id = shard_doc['_id']
                 
-                if shard_set.has_key(shard_id):
+                if self.shard_set.has_key(shard_id):
                     continue
 
                 shard_conn = Connection(shard_doc['host'])
-		if type(shard_conn) is not Connection:
-			print 'shard_conn failure'
-			print shard_conn
-			return
+                self.assertTrue(isinstance(shard_conn, Connection))
                 oplog_coll = shard_conn['local']['oplog.rs']
                 
                 doc_manager = SolrDocManager('http://127.0.0.1:8080/solr/')
                 oplog = OplogThread(shard_conn, self.address, oplog_coll,
                  True, doc_manager, self.oplog_checkpoint, 
-                 {'test.test'}).run() 
-                shard_set[shard_id] = oplog
-             
-            #time.sleep(5) # for testing purposes 
+                 {'test.test'}).run() #later change run to start
+                self.shard_set[shard_id] = oplog
             
        
