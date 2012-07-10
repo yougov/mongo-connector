db0819ad3c810f9a772af6e9d99f7f1d808a854c
Aayush Upadhyay <aayushu@gmail.com>
2012-07-10 14:59:24 -0400

    fixed mongo_internal tests

--- mongo_internal_tester.py
+++ mongo_internal_tester.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 from mongo_internal import Daemon
 
 class MongoInternalTester(unittest.TestCase):
@@ -8,10 +9,14 @@ class MongoInternalTester(unittest.TestCase):
         
 
     def runTest(self):
-        d = Daemon('localhost:27000', 'config.txt')
+        d = Daemon('localhost:27217', 'config.txt')
         self.assertTrue(d.isDaemon())
+        d.start()
         
+        while len(d.shard_set) != 2:
+            time.sleep(2)
         d.stop()
+        
         self.assertFalse(d.canRun)
         for thread in d.shard_set.values():
             self.assertFalse(thread.running)
