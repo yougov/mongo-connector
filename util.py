511aab58b1655f3cb95d8a02f6914785b6858307
Aayush Upadhyay <aayushu@gmail.com>
2012-07-18 12:06:25 -0400

    pep8'd solr_doc_manager and util.py

--- util.py
+++ util.py
@@ -1,13 +1,14 @@
-"""A set of utilities used throughout the mongo-connector 
+"""A set of utilities used throughout the mongo-connector
 """
 
-from pymongo import Connection
-from bson.timestamp import Timestamp
-from urllib2 import urlopen
-import time
 import sys
+import time
 
-    
+from bson.timestamp import Timestamp
+from pymongo import Connection
+from urllib2 import urlopen
+
+
 def verify_url(url):
     """Verifies the validity of a given url.
     """
@@ -16,34 +17,35 @@ def verify_url(url):
         return True
     except:
         return False
-    
+
 
 def bson_ts_to_long(timestamp):
     """Convert BSON timestamp into integer.
-    
-    Conversion rule is based from the specs  
-    (http://bsonspec.org/#/specification). 
+
+    Conversion rule is based from the specs
+    (http://bsonspec.org/#/specification).
     """
     return ((timestamp.time << 32) + timestamp.inc)
-    
-    
+
+
 def long_to_bson_ts(val):
-    """Convert integer into BSON timestamp. 
+    """Convert integer into BSON timestamp.
     """
     seconds = val >> 32
     increment = val & 0xffffffff
-    
+
     return Timestamp(seconds, increment)
 
-def retry_until_ok(func, args = None, no_func = False):
+
+def retry_until_ok(func, args=None, no_func=False):
     """Retry code block until it succeeds.
     """
-    
+
     result = True
-    
+
     while True:
         try:
-            if no_func == True:         #just a statement, not a function
+            if no_func is True:         # just a statement, not a function
                 result = func
                 break
             elif args is None:
@@ -58,5 +60,5 @@ def retry_until_ok(func, args = None, no_func = False):
             print str(args)
             print str(no_func)
             time.sleep(1)
-            
-    return result 
+
+    return result
