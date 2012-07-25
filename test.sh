92f51acc7883769ae0072cc1f867754bb5c4f3d1
Aayush Upadhyay <aayushu@gmail.com>
2012-07-25 12:58:39 -0400

    renamed some files

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-python mongo_internal_tester.py
+python test_mongo_internal.py
 python test_oplog_manager.py 
 python test_oplog_manager_sharded.py
 python test_synchronizer.py
-python util_tester.py
+python test_util.py
