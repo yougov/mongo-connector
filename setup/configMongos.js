97d4eef2138dfea316aaf465bf9148bde0585c0f
Aayush Upadhyay <aayushu@gmail.com>
2012-07-26 12:17:12 -0400

    fixed test scripts to programatically configure shards/mongos

--- setup/configMongos.js
+++
@@ -1,8 +0,0 @@
-//(C) Copyright 2012, 10gen
-
-//=========================================#
-//         Configure Mongos                #
-//=========================================#
-
-db._adminCommand({addShard: "demo-repl/localhost:27117"});
-//printjson(db._adminCommand({enablesharding: "demo-repl/localhost:27117/test"}));
