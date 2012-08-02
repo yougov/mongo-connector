712e8d4bbebf1437c4b82b2ca959dbca0f8d9bd6
stedile <leostedile@gmail.com>
2012-08-02 16:46:06 -0400

    added setup cluster

--- mongo-connector/tests/setup_cluster.py
+++ mongo-connector/tests/setup_cluster.py
@@ -101,10 +101,8 @@ def startMongoProc(port, replSetName, data, log, key_file):
         CMD[0] += " --keyFile " + key_file
 
     CMD[0] += " &"
-    print CMD
     executeCommand(CMD)
-    if int(port) is not (PORTS_ONE['ARBITER'] or PORTS_TWO['ARBITER']):
-        checkStarted(int(port))
+    checkStarted(int(port))
 
 
 def executeCommand(command):
