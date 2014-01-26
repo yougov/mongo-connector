e8abf021cc99d68efc35da5ac7b912da4c1a98c5
Xavier Grand <grand.xavier@gmail.com>
2014-01-26 03:25:44 +0100

    Added filtering on fields copied

--- mongo_connector/doc_managers/algolia_doc_manager.py
+++ mongo_connector/doc_managers/algolia_doc_manager.py
@@ -18,11 +18,14 @@
     This file is a document manager for the Algolia search engine.
     """
 import logging
+import json
 import bson.json_util as bsjson
 
 from algoliasearch import algoliasearch
 from mongo_connector import errors
 from threading import Timer, RLock
+
+decoder = json.JSONDecoder()
 
 class DocManager():
     """The DocManager class creates a connection to the Algolia engine and
@@ -46,18 +49,39 @@ class DocManager():
         self.mutex = RLock()
         self.auto_commit = True
         self.run_auto_commit()
+        try:
+            json = open("algolia_fields.json", 'r')
+            self.attributes_to_copy = decoder.decode(json.read())
+            if self.attributes_to_copy:
+                self.attributes_to_copy += [ '_id', '_ts']
+            logging.info("Algolia Connector: Start with filter.")
+        except IOError: # No filter file
+            self.attributes_to_copy = None
+            logging.info("Algolia Connector: Start without filter.")
 
     def stop(self):
         """ Stops the instance
         """
         self.auto_commit = False
 
+    def filter(self, doc):
+        if not self.attributes_to_copy:
+            return doc
+
+        filtered_doc = {}
+        for key, value in doc.items():
+            if key in self.attributes_to_copy:
+                filtered_doc[key] = value
+        return filtered_doc
+
+
     def upsert(self, doc):
         """ Update or insert a document into Algolia
         """
         with self.mutex:
-            self.last_object_id = doc['objectID'] = str(doc[self.unique_key])
-            doc[self.unique_key] = str(doc[self.unique_key]) # mongodb ObjectID is not serializable
+            self.last_object_id = str(doc[self.unique_key]) # mongodb ObjectID is not serializable
+            doc = self.filter(doc)
+            doc[self.unique_key] = doc['objectID'] = self.last_object_id
             self.batch.append({ 'action': 'addObject', 'body': doc })
             if len(self.batch) >= DocManager.BATCH_SIZE:
                 self.commit()
