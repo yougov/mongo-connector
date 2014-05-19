2e246848a132d7cb59da3d38c5610f86d5333746
Honza Král <honza.kral@gmail.com>
2014-05-19 13:23:56 +0200

    Elasticsearch test refactorings
    Changes:
     abstract connection behavior to ElasticsearchTestCase
     move _remove and _search methods to ElasticsearchTestCase
     remove index in tearDown to avoid test dependency
    
    Related fixes:
     get_last_doc now runs on empty index correctly with latest ES
     test suite now always connects to the configured ES cluster

--- tests/test_elastic.py
+++ tests/test_elastic.py
@@ -25,8 +25,7 @@ else:
 
 sys.path[0:0] = [""]
 
-from elasticsearch import Elasticsearch, exceptions as es_exceptions
-from elasticsearch.client import IndicesClient
+from elasticsearch import Elasticsearch
 from pymongo import MongoClient
 
 from tests import elastic_pair, mongo_host, STRESS_COUNT
@@ -37,12 +36,35 @@ from tests.setup_cluster import (start_replica_set,
 from mongo_connector.doc_managers.elastic_doc_manager import DocManager
 from mongo_connector.connector import Connector
 from mongo_connector.util import retry_until_ok
-from mongo_connector.errors import OperationFailed
 from pymongo.errors import OperationFailure, AutoReconnect
 from tests.util import assert_soon
 
 
-class TestElastic(unittest.TestCase):
+class ElasticsearchTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.elastic_conn = Elasticsearch(hosts=[elastic_pair])
+        cls.elastic_doc = DocManager(elastic_pair,
+                                     auto_commit_interval=0)
+
+    def setUp(self):
+        # Create target index in elasticsearch
+        self.elastic_conn.indices.create(index='test.test')
+        self.elastic_conn.cluster.health(wait_for_status='yellow', index='test.test')
+
+    def tearDown(self):
+        self.elastic_conn.indices.delete(index='test.test', ignore=404)
+
+    def _search(self):
+        return self.elastic_doc._stream_search(index="test.test",
+                                   body={"query": {"match_all": {}}})
+
+    def _remove(self):
+        self.elastic_conn.indices.delete_mapping(index="test.test",
+                                    doc_type=self.elastic_doc.doc_type)
+        self.elastic_conn.indices.refresh(index="test.test")
+
+class TestElastic(ElasticsearchTestCase):
     """ Tests the Elastic instance
     """
 
@@ -50,7 +72,7 @@ class TestElastic(unittest.TestCase):
     def setUpClass(cls):
         """ Starts the cluster
         """
-        cls.elastic_doc = DocManager(elastic_pair, auto_commit=False)
+        super(TestElastic, cls).setUpClass()
         _, cls.secondary_p, cls.primary_p = start_replica_set('test-elastic')
         cls.conn = MongoClient(mongo_host, cls.primary_p,
                                replicaSet='test-elastic')
@@ -64,11 +86,13 @@ class TestElastic(unittest.TestCase):
     def tearDown(self):
         """ Ends the connector
         """
+        super(TestElastic, self).tearDown()
         self.connector.join()
 
     def setUp(self):
         """ Starts a new connector for every test
         """
+        super(TestElastic, self).setUp()
         try:
             os.unlink("config.txt")
         except OSError:
@@ -84,22 +108,11 @@ class TestElastic(unittest.TestCase):
             doc_manager='mongo_connector/doc_managers/elastic_doc_manager.py',
             auto_commit_interval=0
         )
-        # Clean out test databases
-        try:
-            self.elastic_doc._remove()
-        except OperationFailed:
-            try:
-                # Create test.test index if necessary
-                client = Elasticsearch(hosts=[elastic_pair])
-                idx_client = IndicesClient(client)
-                idx_client.create(index='test.test')
-            except es_exceptions.TransportError:
-                pass
 
         self.conn.test.test.drop()
         self.connector.start()
         assert_soon(lambda: len(self.connector.shard_set) > 0)
-        assert_soon(lambda: sum(1 for _ in self.elastic_doc._search()) == 0)
+        assert_soon(lambda: sum(1 for _ in self._search()) == 0)
 
     def test_shard_length(self):
         """Tests the shard_length to see if the shard set was recognized
@@ -113,8 +126,8 @@ class TestElastic(unittest.TestCase):
         """
 
         self.conn['test']['test'].insert({'name': 'paulie'})
-        assert_soon(lambda: sum(1 for _ in self.elastic_doc._search()) > 0)
-        result_set_1 = list(self.elastic_doc._search())
+        assert_soon(lambda: sum(1 for _ in self._search()) > 0)
+        result_set_1 = list(self._search())
         self.assertEqual(len(result_set_1), 1)
         result_set_2 = self.conn['test']['test'].find_one()
         for item in result_set_1:
@@ -126,10 +139,10 @@ class TestElastic(unittest.TestCase):
         """
 
         self.conn['test']['test'].insert({'name': 'paulie'})
-        assert_soon(lambda: sum(1 for _ in self.elastic_doc._search()) == 1)
+        assert_soon(lambda: sum(1 for _ in self._search()) == 1)
         self.conn['test']['test'].remove({'name': 'paulie'})
-        assert_soon(lambda: sum(1 for _ in self.elastic_doc._search()) != 1)
-        self.assertEqual(sum(1 for _ in self.elastic_doc._search()), 0)
+        assert_soon(lambda: sum(1 for _ in self._search()) != 1)
+        self.assertEqual(sum(1 for _ in self._search()), 0)
 
     def test_rollback(self):
         """Tests rollback. We force a rollback by adding a doc, killing the
@@ -142,7 +155,7 @@ class TestElastic(unittest.TestCase):
         self.conn['test']['test'].insert({'name': 'paul'})
         condition1 = lambda: self.conn['test']['test'].find(
             {'name': 'paul'}).count() == 1
-        condition2 = lambda: sum(1 for _ in self.elastic_doc._search()) == 1
+        condition2 = lambda: sum(1 for _ in self._search()) == 1
         assert_soon(condition1)
         assert_soon(condition2)
 
@@ -155,8 +168,8 @@ class TestElastic(unittest.TestCase):
         time.sleep(5)
         retry_until_ok(self.conn.test.test.insert,
                        {'name': 'pauline'})
-        assert_soon(lambda: sum(1 for _ in self.elastic_doc._search()) == 2)
-        result_set_1 = list(self.elastic_doc._search())
+        assert_soon(lambda: sum(1 for _ in self._search()) == 2)
+        result_set_1 = list(self._search())
         result_set_2 = self.conn['test']['test'].find_one({'name': 'pauline'})
         self.assertEqual(len(result_set_1), 2)
         #make sure pauline is there
@@ -172,7 +185,7 @@ class TestElastic(unittest.TestCase):
         restart_mongo_proc(self.secondary_p)
 
         time.sleep(2)
-        result_set_1 = list(self.elastic_doc._search())
+        result_set_1 = list(self._search())
         self.assertEqual(len(result_set_1), 1)
         for item in result_set_1:
             self.assertEqual(item['name'], 'paul')
@@ -185,11 +198,11 @@ class TestElastic(unittest.TestCase):
         for i in range(0, STRESS_COUNT):
             self.conn['test']['test'].insert({'name': 'Paul ' + str(i)})
         time.sleep(5)
-        search = self.elastic_doc._search
+        search = self._search
         condition = lambda: sum(1 for _ in search()) == STRESS_COUNT
         assert_soon(condition)
         for i in range(0, STRESS_COUNT):
-            result_set_1 = self.elastic_doc._search()
+            result_set_1 = self._search()
             for item in result_set_1:
                 if(item['name'] == 'Paul' + str(i)):
                     self.assertEqual(item['_id'], item['_id'])
@@ -202,7 +215,7 @@ class TestElastic(unittest.TestCase):
         for i in range(0, STRESS_COUNT):
             self.conn['test']['test'].insert({'name': 'Paul ' + str(i)})
 
-        search = self.elastic_doc._search
+        search = self._search
         condition = lambda: sum(1 for _ in search()) == STRESS_COUNT
         assert_soon(condition)
         primary_conn = MongoClient(mongo_host, self.primary_p)
@@ -222,9 +235,9 @@ class TestElastic(unittest.TestCase):
                     {'name': 'Pauline ' + str(count)})
             except (OperationFailure, AutoReconnect):
                 time.sleep(1)
-        assert_soon(lambda: sum(1 for _ in self.elastic_doc._search())
+        assert_soon(lambda: sum(1 for _ in self._search())
                     == self.conn['test']['test'].find().count())
-        result_set_1 = self.elastic_doc._search()
+        result_set_1 = self._search()
         for item in result_set_1:
             if 'Pauline' in item['name']:
                 result_set_2 = self.conn['test']['test'].find_one(
@@ -238,11 +251,11 @@ class TestElastic(unittest.TestCase):
         assert_soon(lambda: db_admin.command("isMaster")['ismaster'])
         restart_mongo_proc(self.secondary_p)
 
-        search = self.elastic_doc._search
+        search = self._search
         condition = lambda: sum(1 for _ in search()) == STRESS_COUNT
         assert_soon(condition)
 
-        result_set_1 = list(self.elastic_doc._search())
+        result_set_1 = list(self._search())
         self.assertEqual(len(result_set_1), STRESS_COUNT)
         for item in result_set_1:
             self.assertTrue('Paul' in item['name'])
