1a71d146550348e1a5e373d17d4a14771f7dd49d
Luke Lovett <luke.lovett@mongodb.com>
2015-06-30 12:56:13 -0700

    version -> dev0

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ extra_opts['cmdclass'] = {
 }
 
 setup(name='mongo-connector',
-      version="2.1",
+      version="2.2.dev0",
       author="MongoDB, Inc.",
       author_email='mongodb-user@googlegroups.com',
       description='Mongo Connector',
