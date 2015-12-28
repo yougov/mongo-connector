124ad4a6129eb01173fd615acecc4a69838e64c8
Akshay Goel <akshay@moengage.com>
2015-12-28 20:11:38 +0530

    Removed unnecessary logs

--- mongo_connector/oplog_manager.py
+++ mongo_connector/oplog_manager.py
@@ -212,7 +212,7 @@ class OplogThread(threading.Thread):
                         # shouldn't be replicated. This may nullify
                         # the document if there's nothing to do.
                         if not self.filter_oplog_entry(entry):
-                            LOG.warning("OplogThread: Nullified entry: %r" % entry)
+                            LOG.info("OplogThread: Nullified entry: %r" % entry)
                             continue
 
                         # sync the current oplog operation
@@ -244,7 +244,7 @@ class OplogThread(threading.Thread):
                         timestamp = util.bson_ts_to_long(entry['ts'])
                         for docman in self.doc_managers:
                             try:
-                                LOG.warning("OplogThread: Operation for this entry is %s and action is %r" % (str(operation), entry['o']))
+                                LOG.info("OplogThread: Operation for this entry is %s and action is %r" % (str(operation), entry['o']))
 
                                 # Remove
                                 if operation == 'd':
