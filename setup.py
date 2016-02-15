382a3369703b8857611108a05c3e941122eb34d0
aherlihy <anna.herlihy@10gen.com>
2016-02-15 13:48:20 +0100

    BUMP 2.2

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ extra_opts['cmdclass'] = {
 }
 
 setup(name='mongo-connector',
-      version="2.2.dev0",
+      version="2.2",
       author="MongoDB, Inc.",
       author_email='mongodb-user@googlegroups.com',
       description='Mongo Connector',
