406ca38d33c3db64adfe6feeb0b12153d1d88742
Shane Harvey <shane.harvey@mongodb.com>
2017-02-10 16:49:52 -0800

    Start work on 2.5.1

--- mongo_connector/constants.py
+++ mongo_connector/constants.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 
-__version__ = '2.5.0'
+__version__ = '2.5.1.dev0'
 
 # Maximum # of documents to process before recording timestamp
 # default = -1 (no maximum)
