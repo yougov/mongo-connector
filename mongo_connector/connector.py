5fe85668babd79b65af11356d6e1555e22a73dd9
Shane Harvey <shane.harvey@mongodb.com>
2017-02-13 10:30:09 -0800

    Bug fix: pymongo 3.0 defines 'version' instead of '__version__' (#649)

--- mongo_connector/connector.py
+++ mongo_connector/connector.py
@@ -1189,7 +1189,11 @@ def log_startup_info():
                     __version__)
     LOG.always('Python version: %s', sys.version)
     LOG.always('Platform: %s', platform.platform())
-    LOG.always('pymongo version: %s', pymongo.__version__)
+    if hasattr(pymongo, '__version__'):
+        pymongo_version = pymongo.__version__
+    else:
+        pymongo_version = pymongo.version
+    LOG.always('pymongo version: %s', pymongo_version)
     if not pymongo.has_c():
         LOG.warning(
             'pymongo version %s was installed without the C extensions. '
