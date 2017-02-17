7014bfb3f4ba6438a2acdbc948428431ff5ac8da
Shane Harvey <shane.harvey@mongodb.com>
2017-02-17 10:30:07 -0800

    Update $unset test cases

--- tests/test_doc_manager_base.py
+++ tests/test_doc_manager_base.py
@@ -16,9 +16,12 @@ import sys
 
 sys.path[0:0] = [""]
 
+from mongo_connector.connector import (get_mininum_mongodb_version,
+                                       update_mininum_mongodb_version)
 from mongo_connector.doc_managers.doc_manager_base import (DocManagerBase,
                                                            UpdateDoesNotApply)
 from mongo_connector.test_utils import TESTARGS
+from mongo_connector.version import Version
 
 from tests import unittest
 
@@ -49,43 +52,6 @@ UPDATE_SUCCESS_TEST_CASES = [
         "result": {"a": 1, "b": [0, None, 2, 3]}
     },
     {
-        "description": "Update by un-setting a non-existent field.",
-        "doc": {"a": 1, "b": 2},
-        "update_spec": {"$unset": {"not-present": True}},
-        "result": {"a": 1, "b": 2}
-    },
-    {
-        "description": "Update by un-setting a non-existent nested field.",
-        "doc": {"a": 1, "b": {"c": {"d": 1}}},
-        "update_spec": {"$unset": {"b.not-present.foo": True}},
-        "result": {"a": 1, "b": {"c": {"d": 1}}}
-    },
-    {
-        "description": "Update by un-setting invalid array index.",
-        "doc": {"a": 1, "b": [0, 1, 2, 3]},
-        "update_spec": {"$unset": {"b.not-an-index": True}},
-        "result": {"a": 1, "b": [0, 1, 2, 3]}
-    },
-    {
-        "description": "Update by un-setting invalid nested array index.",
-        "doc": {"a": 1, "b": [0, 1, 2, 3]},
-        "update_spec": {"$unset": {"b.not-an-index.not-present": True}},
-        "result": {"a": 1, "b": [0, 1, 2, 3]}
-    },
-    {
-        "description": "Update by un-setting a non-existent array element.",
-        "doc": {"a": 1, "b": [0, 1, 2]},
-        "update_spec": {"$unset": {"b.4": True}},
-        "result": {"a": 1, "b": [0, 1, 2]}
-    },
-    {
-        "description": "Update by un-setting a non-existent field in an array"
-                       "element.",
-        "doc": {"a": 1, "b": [0, {"c": 1}, 2]},
-        "update_spec": {"$unset": {"b.1.not-present": True}},
-        "result": {"a": 1, "b": [0, {"c": 1}, 2]}
-    },
-    {
         "description": "Update by adding a field.",
         "doc": {"a": 1},
         "update_spec": {"$set": {"b": [{"c": 1}, {"d": 2}]}},
@@ -98,11 +64,11 @@ UPDATE_SUCCESS_TEST_CASES = [
         "result": {"a": 1, "b": {"c": {"d": 2}}}
     },
     {
-        "description": "Update by adding and removing a field.",
-        "doc": {"a": 1},
-        "update_spec": {"$unset": {"a": True, "not-present": True},
-                        "$set": {"b": 2}},
-        "result": {"b": 2}
+        "description": "Update by adding and removing a fields.",
+        "doc": {"a": 1, "c": 3},
+        "update_spec": {"$unset": {"a": True, "c": True},
+                        "$set": {"b": 2, "d": 4}},
+        "result": {"b": 2, "d": 4}
     },
     {
         "description": "Update by setting an element far beyond the end of"
@@ -159,6 +125,53 @@ UPDATE_SUCCESS_TEST_CASES = [
     },
 ]
 
+UNSET_FAILURE_TEST_CASES = [
+    {
+        "description": "Update by un-setting a non-existent field.",
+        "doc": {"a": 1, "b": 2},
+        "update_spec": {"$unset": {"not-present": True}},
+        "result": {"a": 1, "b": 2}
+    },
+    {
+        "description": "Update by un-setting a non-existent nested field.",
+        "doc": {"a": 1, "b": {"c": {"d": 1}}},
+        "update_spec": {"$unset": {"b.not-present.foo": True}},
+        "result": {"a": 1, "b": {"c": {"d": 1}}}
+    },
+    {
+        "description": "Update by un-setting invalid array index.",
+        "doc": {"a": 1, "b": [0, 1, 2, 3]},
+        "update_spec": {"$unset": {"b.not-an-index": True}},
+        "result": {"a": 1, "b": [0, 1, 2, 3]}
+    },
+    {
+        "description": "Update by un-setting invalid nested array index.",
+        "doc": {"a": 1, "b": [0, 1, 2, 3]},
+        "update_spec": {"$unset": {"b.not-an-index.not-present": True}},
+        "result": {"a": 1, "b": [0, 1, 2, 3]}
+    },
+    {
+        "description": "Update by un-setting a non-existent array element.",
+        "doc": {"a": 1, "b": [0, 1, 2]},
+        "update_spec": {"$unset": {"b.4": True}},
+        "result": {"a": 1, "b": [0, 1, 2]}
+    },
+    {
+        "description": "Update by un-setting a non-existent field in an array"
+                       "element.",
+        "doc": {"a": 1, "b": [0, {"c": 1}, 2]},
+        "update_spec": {"$unset": {"b.1.not-present": True}},
+        "result": {"a": 1, "b": [0, {"c": 1}, 2]}
+    },
+    {
+        "description": "Update by adding and removing a non-existent field.",
+        "doc": {"a": 1},
+        "update_spec": {"$unset": {"a": True, "not-present": True},
+                        "$set": {"b": 2}},
+        "result": {"b": 2}
+    }
+]
+
 UPDATE_FAILURE_TEST_CASES = [
     {
         "description": "Using array notation on non-array field.",
@@ -199,23 +212,38 @@ class TestDocManagerBase(unittest.TestCase):
     def setUp(self):
         self.base = DocManagerBase()
 
+    def assertUpdateTestSucceeds(self, test):
+        self.assertEqual(
+            self.base.apply_update(test["doc"], test["update_spec"]),
+            test["result"], msg=test["description"])
+
+    def assertUpdateTestFails(self, test):
+        try:
+            doc = self.base.apply_update(test["doc"], test["update_spec"])
+            self.fail(
+                "UpdateDoesNotApply on MongoDB verison %s not raised for "
+                "test: %s, applied %r to %r and got %r" % (
+                    get_mininum_mongodb_version(), test["description"],
+                    test["update_spec"], test["doc"], doc))
+        except UpdateDoesNotApply:
+            pass
+
     def test_apply_update(self):
         for test in UPDATE_SUCCESS_TEST_CASES:
-            self.assertEqual(
-                self.base.apply_update(test["doc"], test["update_spec"]),
-                test["result"], msg=test["description"])
+            self.assertUpdateTestSucceeds(test)
 
     def test_apply_update_fails(self):
         for test in UPDATE_FAILURE_TEST_CASES:
-            try:
-                doc = self.base.apply_update(test["doc"], test["update_spec"])
-                self.fail(
-                    "UpdateDoesNotApply not raised for test: %s, "
-                    "applied %r to %r and got %r" % (
-                        test["description"], test["update_spec"],
-                        test["doc"], doc))
-            except UpdateDoesNotApply:
-                pass
+            self.assertUpdateTestFails(test)
+
+    def test_apply_update_unset_failures(self):
+        for mock_mongodb_version in [(3, 4), (3, 2), (3, 0), (2, 6), (2, 4)]:
+            update_mininum_mongodb_version(Version(*mock_mongodb_version))
+            for test in UNSET_FAILURE_TEST_CASES:
+                if mock_mongodb_version == (2, 4):
+                    self.assertUpdateTestSucceeds(test)
+                else:
+                    self.assertUpdateTestFails(test)
 
     def test_bulk_upsert(self):
         with self.assertRaises(NotImplementedError):
